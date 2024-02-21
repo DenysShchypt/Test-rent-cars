7897e89fa0f9e9fff4e12b8cae13f9af79c385c2
DenysShchypt <denysshchyptsovit@gmail.com>
2024-02-21 15:50:15 +0200

    creat folders components Loader and SharedLayout

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
 import './index.css';
+
+const theme = {
+  colors: {
+    grey: 'grey',
+    blue: 'blue',
+    white: 'white',
+    black: 'black',
+    yellow: 'yellow',
+    error: 'red',
+  },
+  spacing: value => `${value * 2}px`,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
       basename="/Test-rent-cars"
     >
-      <App />
+      <ThemeProvider
+        theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
