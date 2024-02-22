74050baf1f5185c610e3bfb4898bc2fefbc76e3d
DenysShchypt <denysshchyptsovit@gmail.com>
2024-02-22 14:24:42 +0200

    add globalStyle

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'components/GlobalStyle';
 
-const theme = {
-  colors: {
-    grey: 'grey',
-    blue: 'blue',
-    white: 'white',
-    black: 'black',
-    yellow: 'yellow',
-    error: 'red',
-  },
-  spacing: value => `${value * 2}px`,
-};
+// const theme = {
+//   colors: {
+//     grey: 'grey',
+//     blue: 'blue',
+//     white: 'white',
+//     black: 'black',
+//     yellow: 'yellow',
+//     error: 'red',
+//   },
+//   spacing: value => `${value * 2}px`,
+// };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/test-rent-cars">
       <App />
+      <GlobalStyle />
     </BrowserRouter>
   </React.StrictMode>
 );
